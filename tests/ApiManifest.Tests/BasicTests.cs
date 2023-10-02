24aeef5d450edaf432335dd5da1f68cd806777e4
Peter Ombwa <peter.ombwa@microsoft.com>
2023-10-02 15:47:59 -0700

    chore: Update solution.

---
+++ tests/ApiManifest.Tests/BasicTests.cs
@@ -0,0 +1,201 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Microsoft.OpenApi.ApiManifest.Tests;
+public class BasicTests
+{
+    private readonly ApiManifestDocument exampleApiManifest;
+    public BasicTests()
+    {
+        exampleApiManifest = CreateDocument();
+    }
+
+    // Create test to instantiate a simple ApiManifestDocument
+    [Fact]
+    public void InitializeDocument()
+    {
+        Assert.NotNull(exampleApiManifest);
+    }
+
+    // Serialize the ApiManifestDocument to a string
+    [Fact]
+    public void SerializeDocument()
+    {
+        var stream = new MemoryStream();
+        var writer = new Utf8JsonWriter(stream);
+        exampleApiManifest.Write(writer);
+        writer.Flush();
+        // Read string from stream
+        stream.Position = 0;
+        var reader = new StreamReader(stream);
+        var json = reader.ReadToEnd();
+        Debug.WriteLine(json);
+        var doc = JsonDocument.Parse(json);
+        Assert.NotNull(doc);
+        Assert.Equal("application-name", doc.RootElement.GetProperty("applicationName").GetString());
+        Assert.Equal("Microsoft", doc.RootElement.GetProperty("publisher").GetProperty("name").GetString());
+    }
+
+    // Deserialize the ApiManifestDocument from a string
+    [Fact]
+    public void DeserializeDocument()
+    {
+        var stream = new MemoryStream();
+        var writer = new Utf8JsonWriter(stream);
+        exampleApiManifest.Write(writer);
+        writer.Flush();
+        // Read string from stream
+        stream.Position = 0;
+        var reader = new StreamReader(stream);
+        var json = reader.ReadToEnd();
+        var doc = JsonDocument.Parse(json);
+        var apiManifest = ApiManifestDocument.Load(doc.RootElement);
+        Assert.Equivalent(exampleApiManifest.Publisher, apiManifest.Publisher);
+        Assert.Equivalent(exampleApiManifest.ApiDependencies["example"].Requests, apiManifest.ApiDependencies["example"].Requests);
+        Assert.Equivalent(exampleApiManifest.ApiDependencies["example"].ApiDescriptionUrl, apiManifest.ApiDependencies["example"].ApiDescriptionUrl);
+        Assert.Equivalent(exampleApiManifest.ApiDependencies["example"].ApiDeploymentBaseUrl, apiManifest.ApiDependencies["example"].ApiDeploymentBaseUrl);
+        var expectedAuth = exampleApiManifest.ApiDependencies["example"].AuthorizationRequirements;
+        var actualAuth = apiManifest.ApiDependencies["example"].AuthorizationRequirements;
+        Assert.Equivalent(expectedAuth?.ClientIdentifier, actualAuth?.ClientIdentifier);
+        Assert.Equivalent(expectedAuth?.Access?[0]?.Content?.ToJsonString(), actualAuth?.Access?[0]?.Content?.ToJsonString());
+    }
+
+
+    // Create an empty document
+    [Fact]
+    public void CreateDocumentWithRequiredFields()
+    {
+        var doc = new ApiManifestDocument("application-name");
+        Assert.NotNull(doc);
+        Assert.Equal("application-name", doc.ApplicationName);
+        Assert.NotNull(doc.ApiDependencies);
+        Assert.Empty(doc.ApiDependencies);
+    }
+
+    // Create a document with a publisher that is missing required fields (name and contactEmail).
+    [Fact]
+    public void CreateDocumentWithMissingRequiredPublisherFields()
+    {
+        _ = Assert.Throws<ArgumentNullException>(() =>
+        {
+            var doc = new ApiManifestDocument("application-name")
+            {
+                Publisher = new("", "")
+            };
+        }
+        );
+    }
+
+    [Fact]
+    public void FailToParseDocumentWithoutApplicationName()
+    {
+        _ = Assert.Throws<ArgumentNullException>(() =>
+        {
+            var serializedValue = "{\"apiDependencies\": { \"graph\": {\"apiDescriptionUrl\":\"https://example.org\"}}}";
+            var doc = JsonDocument.Parse(serializedValue);
+            _ = ApiManifestDocument.Load(doc.RootElement);
+        }
+        );
+    }
+
+    [Fact]
+    public void ParsesApiDescriptionUrlField()
+    {
+        // Given
+        var serializedValue = "{\"applicationName\": \"application-name\", \"apiDependencies\": { \"graph\": {\"apiDescriptionUrl\":\"https://example.org\"}}}";
+        var doc = JsonDocument.Parse(serializedValue);
+
+        // When
+        var apiManifest = ApiManifestDocument.Load(doc.RootElement);
+
+        // Then
+        Assert.Equal("https://example.org", apiManifest.ApiDependencies["graph"].ApiDescriptionUrl);
+    }
+    [Fact]
+    public void ParseApiDescriptionVersionField()
+    {
+        // Given
+        var serializedValue = "{\"applicationName\": \"application-name\", \"apiDependencies\": { \"graph\": {\"apiDescriptionVersion\":\"v1.0\"}}}";
+        var doc = JsonDocument.Parse(serializedValue);
+
+        // When
+        var apiManifest = ApiManifestDocument.Load(doc.RootElement);
+
+        // Then
+        Assert.Equal("v1.0", apiManifest.ApiDependencies["graph"].ApiDescriptionVersion);
+    }
+    [Fact]
+    public void ParsesApiDeploymentBaseUrl()
+    {
+        // Given
+        var serializedValue = "{\"applicationName\": \"application-name\", \"apiDependencies\": { \"graph\": {\"apiDeploymentBaseUrl\":\"https://example.org/\"}}}";
+        var doc = JsonDocument.Parse(serializedValue);
+
+        // When
+        var apiManifest = ApiManifestDocument.Load(doc.RootElement);
+
+        // Then
+        Assert.Equal("https://example.org/", apiManifest.ApiDependencies["graph"].ApiDeploymentBaseUrl);
+    }
+
+    [Fact]
+    public void ParsesApiDeploymentBaseUrlWithDifferentCasing()
+    {
+        // Given
+        var serializedValue = "{\"applicationName\": \"application-name\", \"apiDependencies\": { \"graph\": {\"APIDeploymentBaseUrl\":\"https://example.org/\"}}}";
+        var doc = JsonDocument.Parse(serializedValue);
+
+        // When
+        var apiManifest = ApiManifestDocument.Load(doc.RootElement);
+
+        // Then
+        Assert.Equal("https://example.org/", apiManifest.ApiDependencies["graph"].ApiDeploymentBaseUrl);
+    }
+
+    [Fact]
+    public void DoesNotFailOnExtraneousProperty()
+    {
+        // Given
+        var serializedValue = "{\"applicationName\": \"application-name\", \"apiDependencies\": { \"graph\": {\"APIDeploymentBaseUrl\":\"https://example.org/\", \"APISensitivity\":\"low\"}}}";
+        var doc = JsonDocument.Parse(serializedValue);
+
+        // When
+        var apiManifest = ApiManifestDocument.Load(doc.RootElement);
+
+        // Then
+        Assert.Equal("https://example.org/", apiManifest.ApiDependencies["graph"].ApiDeploymentBaseUrl);
+    }
+
+    private static ApiManifestDocument CreateDocument()
+    {
+        return new ApiManifestDocument("application-name")
+        {
+            Publisher = new("Microsoft", "example@example.org"),
+            ApiDependencies = new() {
+                { "example", new()
+                    {
+                        ApiDescriptionUrl = "https://example.org",
+                        ApiDeploymentBaseUrl = "https://example.org/v1.0/",
+                        AuthorizationRequirements = new()
+                        {
+                            ClientIdentifier = "1234",
+                            Access = new() {
+                                new () { Type= "application", Content = new JsonObject() {
+                                        { "scopes", new JsonArray() {"User.Read.All"} }}
+                                     } ,
+                                new () { Type= "delegated", Content = new JsonObject() {
+                                        { "scopes", new JsonArray() {"User.Read", "Mail.Read"} }}
+                                     }
+                            }
+                        },
+                        Requests = new() {
+                            new () { Method = "GET", UriTemplate = "/api/v1/endpoint" },
+                            new () { Method = "POST", UriTemplate = "/api/v1/endpoint"}
+                        }
+                    }
+                }
+            }
+        };
+    }
+}
---
+++ tests/ApiManifest.Tests/CreateTests.cs
@@ -0,0 +1,92 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Microsoft.OpenApi.ApiManifest.Tests;
+public class CreateTests
+{
+
+    [Fact]
+    public void CreateApiManifestDocumentWithRequiredFields()
+    {
+        var apiManifest = new ApiManifestDocument("application-name");
+        Assert.NotNull(apiManifest);
+        Assert.Equal("application-name", apiManifest.ApplicationName);
+        Assert.Null(apiManifest.Publisher);
+        Assert.Empty(apiManifest.ApiDependencies);
+        Assert.Empty(apiManifest.Extensions);
+    }
+
+    [Theory]
+    [InlineData("foo@bar")]
+    [InlineData("foo@bar.com")]
+    public void CreatePublisher(string contactEmail)
+    {
+        var publisher = new Publisher(name: "Contoso", contactEmail: contactEmail);
+        Assert.Equal("Contoso", publisher.Name);
+        Assert.Equal(contactEmail, publisher.ContactEmail);
+
+    }
+
+    [Theory]
+    [InlineData("foo")]
+    [InlineData("foo@")]
+    [InlineData("foo@@bar.com")]
+    [InlineData("foo @bar.com")]
+    public void CreatePublisherWithInvalidEmail(string contactEmail)
+    {
+        _ = Assert.Throws<ArgumentException>(() =>
+        {
+            var publisher = new Publisher(name: "Contoso", contactEmail: contactEmail);
+        }
+        );
+    }
+
+    [Theory]
+    [InlineData("foo")]
+    [InlineData("https://foo.com")]
+    [InlineData("http://128.0.0.0")]
+    [InlineData("https://foo@@bar.com")]
+    [InlineData("https://foo bar.com/")]
+    [InlineData("https://graph.microsoft.com/v1.0")]
+    public void CreateApiDependencyWithInvalidApiDeploymentBaseUrl(string apiDeploymentBaseUrl)
+    {
+        _ = Assert.Throws<ArgumentException>(() =>
+        {
+            var apiDependency = new ApiDependency
+            {
+                ApiDeploymentBaseUrl = apiDeploymentBaseUrl
+            };
+        }
+        );
+    }
+
+    // Create test to instantiate ApiManifest with auth
+    [Fact]
+    public void CreateApiManifestWithAuthorizationRequirements()
+    {
+        var apiManifest = new ApiManifestDocument("application-name")
+        {
+            Publisher = new(name: "Contoso", contactEmail: "foo@bar.com"),
+            ApiDependencies = new() {
+                { "Contoso.Api", new() {
+                    ApiDeploymentBaseUrl = "https://api.contoso.com/",
+                    AuthorizationRequirements = new() {
+                        ClientIdentifier = "2143234-234324-234234234-234",
+                        Access = new() {
+                            new() { Type = "oauth2",
+                                    Content = new JsonObject() {
+                                                { "scopes", new JsonArray() { "user.read", "user.write" } }
+                                            }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        Assert.NotNull(apiManifest.ApiDependencies["Contoso.Api"].AuthorizationRequirements);
+        Assert.Equal("https://api.contoso.com/", apiManifest.ApiDependencies["Contoso.Api"].ApiDeploymentBaseUrl);
+        Assert.Equal("2143234-234324-234234234-234", apiManifest?.ApiDependencies["Contoso.Api"]?.AuthorizationRequirements?.ClientIdentifier);
+        Assert.Equal("oauth2", apiManifest?.ApiDependencies["Contoso.Api"]?.AuthorizationRequirements?.Access?[0].Type);
+    }
+
+}
